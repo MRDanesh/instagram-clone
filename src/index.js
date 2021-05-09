61552877e59a073fbab2e43275c81acac70126cf
MRDanesh <Mohammadrezad126@gmail.com>
2021-05-09 13:25:33 -0400

    connect firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import FirebaseContext from './context/firebase';
+import {firebase, FieldValue} from './lib/firebase';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={{firebase, FieldValue}}>
+        <App/>
+    </FirebaseContext.Provider>, document.querySelector('#root')
+);
 
---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyC5Ap0pfuRNFmpzsTwy7cFjoiGMg46nwfM",
+    authDomain: "instagram-146eb.firebaseapp.com",
+    projectId: "instagram-146eb",
+    storageBucket: "instagram-146eb.appspot.com",
+    messagingSenderId: "68087648677",
+    appId: "1:68087648677:web:818272b315c21f8ba0d84c",
+    measurementId: "G-3C7XZ95XJ3"
+};
+
+const firebase = Firebase.initializeApp(config);
+const {FieldValue} = Firebase.firestore;
+
+console.log('firebase', firebase);
+
+export {firebase, FieldValue};
